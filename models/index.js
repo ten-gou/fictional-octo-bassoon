5bc98ea185741c2db49517aad084753294bb6c9c
Maribel <lwang79@zoho.com>
2022-05-09 00:01:57 -0700

    updating file names to be conisitent for heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import all models
-const Post = require('./Post');
-const User = require('./User');
+const Post = require('./post');
+const User = require('./user');
 const Vote = require('./Vote');
 const Comment = require('./Comment');
 
